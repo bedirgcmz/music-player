9f4493eca1f229b5a3fc1effa3a340dded23c3da
bedirgcmz <bdrgcmz@gmail.com>
2023-12-19 18:31:46 +0100

    created project

---
+++ app.js
@@ -0,0 +1,28 @@
+// const player = new MusicPlayer(musicList);
+
+// let music = player.getMusic();
+// console.log(music.getName());
+
+// player.next();
+// music = player.getMusic();
+// console.log(music.getName());
+
+// player.previous();
+// music = player.getMusic();
+// console.log(music.getName());
+
+// player.next();
+// music = player.getMusic();
+// console.log(music.getName());
+
+// player.next();
+// music = player.getMusic();
+// console.log(music.getName());
+
+// player.next();
+// music = player.getMusic();
+// console.log(music.getName());
+
+const player = new MusicPlayer(musicList);
+
+console.log(player);
---
+++ music.js
@@ -0,0 +1,34 @@
+// class Music {
+//     constructor(title, singer, img, file) {
+//         this.title = title;
+//         this.singer = singer;
+//         this.img = img;
+//         this.file = file;
+//     }
+
+//     getName() {
+//         return this.title + " - " + this.singer;
+//     }
+// }
+
+// const musicList = [
+//     new Music("Boşver", "Nilüfer","1.jpeg","1.mp3"),
+//     new Music("Bu da Geçer mi Sevgilim", "Yalın","2.jpeg","2.mp3"),
+//     new Music("Aramızda Uçurumlar", "Suat Suna","3.jpeg","3.mp3")
+// ];
+
+class Music {
+  constructor(title, singer, img, file) {
+    (this.title = title), (this.singer = singer), (this.img = img), (this.file = file);
+  }
+
+  getNme() {
+    return this.title + " - " + this.singer;
+  }
+}
+
+const musicList = [
+  new Music("Bosver", "Nilufer", "1.jpeg", "1.mp3"),
+  new Music("Bu da Geçer mi Sevgilim", "Yalın", "2.jpeg", "2.mp3"),
+  new Music("Aramızda Uçurumlar", "Suat Suna", "3.jpeg", "3.mp3"),
+];
